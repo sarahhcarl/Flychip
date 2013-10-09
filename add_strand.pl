f7a13cf1588717793fa479e4c92d7c93becc17aa
Sarah Carl <sarahhcarl@gmail.com>
2013-10-09 22:29:52 +0100

    Correct version of make_fasta_by_enhancer

--- add_strand.pl
+++ add_strand.pl
@@ -1,8 +1,8 @@
 use strict;
 use warnings;
 
-open FILE, "<nonSox_enhancers.bed";
-open NEWFILE, ">nonSox_enhancers_dmel_strand.bed";
+open FILE, "<nonCNS_enhancers.bed";
+open NEWFILE, ">nonCNS_enhancers_dmel_strand.bed";
 while (my $line = <FILE>) {
 	chomp $line;
 	print NEWFILE $line."\t+\n";
